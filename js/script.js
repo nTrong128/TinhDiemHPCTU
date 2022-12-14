4af77e214264d86e053abe6b9c5d95bef31dcc4a
Vitris128 <lenhattrong128@gmail.com>
2022-12-15 01:54:36 +0700

    picture and timer

--- js/script.js
+++ js/script.js
@@ -85,43 +85,28 @@ modalBg.addEventListener('click', () => {
 })
 
 // Timer
-// Set the date we're counting down to
 var countDownDate = new Date("Jan 22, 2023 00:00:00").getTime();
-
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
   document.getElementById("day-count").innerHTML = days ;
   document.getElementById("hour-count").innerHTML = hours ;
   document.getElementById("minute-count").innerHTML = minutes ;
   document.getElementById("second-count").innerHTML = seconds ;
-//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "HẸN GẶP LẠI NĂM SAU NHÉ";
   }
 }, 1000);
-
-// Get the button
 let mybutton = document.getElementById("backToTopBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -131,8 +116,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
